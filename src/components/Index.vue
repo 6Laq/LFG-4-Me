74858e8276a7e95c2cbd41d4bf881f8f95de7066
corn rolla <gwooseduck@gmail.com>
2019-09-02 10:06:48 -0700

    feat(home): added layout

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,11 +1,41 @@
 <template>
-  <div><h1>oi</h1></div>
+  <div class="page">
+    <Nav />
+    <div class="container column">
+      <div class="side-menu">
+        <div class="side-image"></div>
+      </div>
+      <div class="main-area">
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Nav from './Nav'
+
 export default {
+  name: 'Index',
+  components: {
+    Nav
+  }
 }
 </script>
 
 <style scoped>
+.page {
+  background-color: aliceblue
+}
+.container {
+  background-color: burlywood
+}
+.side-menu {
+  background-color: thistle
+}
+.main-area {
+  background-color: blue
+}
 </style>
